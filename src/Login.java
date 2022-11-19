f8353e98a29299a2e83e318aabcf24478b38e624
Andreas Bratakos <46324674+8180195@users.noreply.github.com>
2022-11-19 18:36:50 +0200

    Login
    Θέλει δουλίτσα.

---
+++ src/Login.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+public class Login {
+    public Login()   {} 
+    String newUsername;
+    String newPassword;
+    ArrayList<Profile>users = new ArrayList<>;
+    public Login(String newUsername, String newPassword, ArrayList<Profile>users){
+        this.newUsername = newUsername;
+        this.newPassword = newPassword;
+        this.user = user;
+    
+  public Boolean checkLogin(newUsername, newPassword, Users) {
+    for (int i=0; i<user.length; i++) {
+        if (newUsername || newPassword == user[i]) {
+               return true;
+         System.out.println("Welcome"); } else {
+            return false;
+        } Systerm.out.println("Wrong Username or password");
+       }
+  }
+    
+    }
+}
